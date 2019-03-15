12f4038830eb0809e26f3e6395bd62233c6509df
Brian Muhia <poppingtonic@gmail.com>
2019-03-16 01:59:10 +0300

    Make completion conditional on prev. command's success

--- project.sh
+++ project.sh
@@ -1,7 +1,7 @@
 . /opt/cocohub/venv/bin/activate
 language="$1"
-python create_project_data.py --language $language
-pbs create_project --project projects/"$language"_project.json
-pbs update_project --project projects/"$language"_project.json --task-presenter projects/"$language"_template.html --tutorial projects/"$language"_tutorial.html --long-description projects/"$language"_long_description.md
-pbs add_tasks --project projects/"$language"_project.json --task-file projects/"$language"_tasks.csv
-rm projects/"$language"_tasks.csv
+python create_project_data.py --language $language && \
+    pbs create_project --project projects/"$language"_project.json && \
+    pbs update_project --project projects/"$language"_project.json --task-presenter projects/"$language"_template.html --tutorial projects/"$language"_tutorial.html --long-description projects/"$language"_long_description.md && \
+    pbs add_tasks --project projects/"$language"_project.json --task-file projects/"$language"_tasks.csv && \
+    rm projects/"$language"_tasks.csv
