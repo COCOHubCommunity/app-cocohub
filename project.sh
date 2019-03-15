5e20e0423bbfc94ce9c52df2106219f9c4d379a2
Brian Muhia <poppingtonic@gmail.com>
2019-03-16 02:37:21 +0300

    Simplify creator

--- project.sh
+++ project.sh
@@ -7,7 +7,7 @@ long_description="$language"_long_description.md
 tasks="$language"_tasks.csv
 
 python create_project_data.py --language $language && \
-    pbs create_project --project projects/$project && \
-    pbs update_project --project projects/$project --task-presenter projects/$template --tutorial projects/$tutorial --long-description projects/$long_description && \
-    pbs add_tasks --project projects/$project --task-file projects/$tasks && \
+    pbs create_project && \
+    pbs update_project && \
+    pbs add_tasks --task-file projects/$tasks && \
     rm projects/$tasks
