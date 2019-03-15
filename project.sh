809194431759552db567e4769f24fb0285433846
Brian Muhia <poppingtonic@gmail.com>
2019-03-16 02:04:03 +0300

    Make names valid for python script

--- project.sh
+++ project.sh
@@ -1,7 +1,13 @@
 . /opt/cocohub/venv/bin/activate
 language="$1"
+project="$language"_project.json
+template="$language"_template.html
+tutorial="$language"_tutorial.html
+long_description="$language"_long_description.md
+tasks="$language"_tasks.csv
+
 python create_project_data.py --language $language && \
-    pbs create_project --project projects/"$language"_project.json && \
-    pbs update_project --project projects/"$language"_project.json --task-presenter projects/"$language"_template.html --tutorial projects/"$language"_tutorial.html --long-description projects/"$language"_long_description.md && \
-    pbs add_tasks --project projects/"$language"_project.json --task-file projects/"$language"_tasks.csv && \
-    rm projects/"$language"_tasks.csv
+    pbs create_project --project projects/$project && \
+    pbs update_project --project projects/$project --task-presenter projects/$template --tutorial projects/$tutorial --long-description projects/$long_description && \
+    pbs add_tasks --project projects/$project --task-file projects/$tasks && \
+    rm projects/$tasks
